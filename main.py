640475af961758d9ec6ffc082af16be1526738dc
Stoyan Zlatev <stoyanzlatev22@gmail.com>
2024-11-03 23:00:30 +0200

    Create simple Tic Tac Toe game

--- main.py
+++ main.py
@@ -1,28 +1,119 @@
-import pygame
+import pygame as pg
+import sys
+from random import randint
 
-pygame.init()
+WIN_SIZE = 900
+CELL_SIZE = WIN_SIZE // 3
+INF = float('inf')
+vec2 = pg.math.Vector2
+CELL_CENTER = vec2(CELL_SIZE / 2)
 
-color = (255,255,255)
-rect_color = (255, 0, 0)
-position = (0,0)
 
-# CREATING CANVAS
-canvas = pygame.display.set_mode((600,400))
+class TicTacToe:
+    def __init__(self, game):
+        self.game = game
+        self.field_image = self.get_scaled_image(path='./assets/images/field.png', res=[WIN_SIZE] * 2)
+        self.O_image = self.get_scaled_image(path='./assets/images/o.png', res=[CELL_SIZE] * 2)
+        self.X_image = self.get_scaled_image(path='./assets/images/x.png', res=[CELL_SIZE] * 2)
 
-# TITLE OF CANVAS
-pygame.display.set_caption("Show Image")
+        self.game_array = [[INF, INF, INF],
+                           [INF, INF, INF],
+                           [INF, INF, INF]]
+        self.player = randint(0, 1)
 
-image = pygame.image.load("./assets/images/background.jpg")
-exit = False
+        self.line_indices_array = [[(0, 0), (0, 1), (0, 2)],
+                                   [(1, 0), (1, 1), (1, 2)],
+                                   [(2, 0), (2, 1), (2, 2)],
+                                   [(0, 0), (1, 0), (2, 0)],
+                                   [(0, 1), (1, 1), (2, 1)],
+                                   [(0, 2), (1, 2), (2, 2)],
+                                   [(0, 0), (1, 1), (2, 2)],
+                                   [(0, 2), (1, 1), (2, 0)]]
+        self.winner = None
+        self.game_steps = 0
+        self.font = pg.font.SysFont('Verdana', CELL_SIZE // 4, True)
 
-while not exit:
-	canvas.fill(color)
-	canvas.blit(image, dest = position)
+    def check_winner(self):
+        for line_indices in self.line_indices_array:
+            sum_line = sum([self.game_array[i][j] for i, j in line_indices])
+            if sum_line in {0, 3}:
+                self.winner = 'XO'[sum_line == 0]
+                self.winner_line = [vec2(line_indices[0][::-1]) * CELL_SIZE + CELL_CENTER,
+                                    vec2(line_indices[2][::-1]) * CELL_SIZE + CELL_CENTER]
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			exit = True
+    def run_game_process(self):
+        current_cell = vec2(pg.mouse.get_pos()) // CELL_SIZE
+        col, row = map(int, current_cell)
+        left_click = pg.mouse.get_pressed()[0]
 
-	pygame.draw.rect(canvas, rect_color,
-					 pygame.Rect(30, 200, 100, 60))
-	pygame.display.update()
+        if left_click and self.game_array[row][col] == INF and not self.winner:
+            self.game_array[row][col] = self.player
+            self.player = not self.player
+            self.game_steps += 1
+            self.check_winner()
+
+    def draw_objects(self):
+        for y, row in enumerate(self.game_array):
+            for x, obj in enumerate(row):
+                if obj != INF:
+                    self.game.screen.blit(self.X_image if obj else self.O_image, vec2(x, y) * CELL_SIZE)
+
+    def draw_winner(self):
+        if self.winner:
+            pg.draw.line(self.game.screen, 'red', *self.winner_line, CELL_SIZE // 8)
+            label = self.font.render(f'Player "{self.winner}" wins!', True, 'white', 'black')
+            self.game.screen.blit(label, (WIN_SIZE // 2 - label.get_width() // 2, WIN_SIZE // 4))
+
+    def draw(self):
+        self.game.screen.blit(self.field_image, (0, 0))
+        self.draw_objects()
+        self.draw_winner()
+
+    @staticmethod
+    def get_scaled_image(path, res):
+        img = pg.image.load(path)
+        return pg.transform.smoothscale(img, res)
+
+    def print_caption(self):
+        pg.display.set_caption(f'Player "{"OX"[self.player]}" turn!')
+        if self.winner:
+            pg.display.set_caption(f'Player "{self.winner}" wins! Press Space to Restart')
+        elif self.game_steps == 9:
+            pg.display.set_caption(f'Game Over! Press Space to Restart')
+
+    def run(self):
+        self.print_caption()
+        self.draw()
+        self.run_game_process()
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode([WIN_SIZE] * 2)
+        self.clock = pg.time.Clock()
+        self.tic_tac_toe = TicTacToe(self)
+
+    def new_game(self):
+        self.tic_tac_toe = TicTacToe(self)
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.new_game()
+
+    def run(self):
+        while True:
+            self.tic_tac_toe.run()
+            self.check_events()
+            pg.display.update()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
